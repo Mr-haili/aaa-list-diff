200daaed77950578bb257e4ce56d2fd7e60e5747
aaa <120000456@qq.com>
2018-02-15 00:24:54 +0800

    update package.json

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { ListDiffer, makeDiffsFromPosList } from './list-differ';
 
 const differ = new ListDiffer();
 let s1, s2;
-s1 = 'The quick brown fox jumps over the lazy dog.';
-s2 = 'Woof';
+s1 = null;
+s2 = null;
 
 const posList = myers(s1, s2);
 console.log('111', posList);
