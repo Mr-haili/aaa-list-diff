449bea71bea0ca1e5ed662f48d4e3353064d18bb
aaa <120000456@qq.com>
2018-02-15 00:01:39 +0800

    add more test cases

--- test/patch.spec.ts
+++ test/patch.spec.ts
@@ -12,22 +12,6 @@ function diffPatchApply(s1: string, s2: string, s3?: string): string {
 	return differ.patchApply(patchs, s3).join('');
 }
 
-// const s1 = 'ABCABBA';
-// const s2 = 'CBABAC';
-// const s1 = "Good dog";
-// const s2 = "Bad dog";
-// const s1 = `
-// I am the very model of a modern Major-General,
-// I've information vegetable, animal, and mineral,
-// I know the kings of England, and I quote the fights historical,
-// From Marathon to Waterloo, in order categorical.`;
-
-// const s2 = `
-// I am the very model of a cartoon individual,
-// My animation's comical, unusual, and whimsical,
-// I'm quite adept at funny gags, comedic theory I have read,
-// From wicked puns and stupid jokes to anvils that drop on your head.`;
-
 describe('patch apply', function(){
 	it('after patch the oldList must equal newList.', function() {
 		let s1: string, s2: string, s3: string, s4: string;
@@ -73,6 +57,29 @@ describe('patch apply', function(){
 
 		s1 = 'y';
 		s2 = 'y123';
+		expect(diffPatchApply(s1, s2)).toEqual(s2);
+
+		// some more test cases
+		s1 = 'ABCABBA';
+		s2 = 'CBABAC';
+		expect(diffPatchApply(s1, s2)).toEqual(s2);
+
+		s1 = "Good dog";
+		s2 = "Bad dog";
+		expect(diffPatchApply(s1, s2)).toEqual(s2);
+
+		s1 = `
+			I am the very model of a modern Major-General,
+			I've information vegetable, animal, and mineral,
+			I know the kings of England, and I quote the fights historical,
+			From Marathon to Waterloo, in order categorical.
+		`;
+		s2 = `
+			I am the very model of a cartoon individual,
+			My animation's comical, unusual, and whimsical,
+			I'm quite adept at funny gags, comedic theory I have read,
+			From wicked puns and stupid jokes to anvils that drop on your head.
+		`;
 		expect(diffPatchApply(s1, s2)).toEqual(s2);
 	});
 });
